5c2dce834c143eab43c41840c3d2fbfc68ebcb4f
Ape <155788660+ApeMV@users.noreply.github.com>
2024-08-05 19:43:31 +0200

    Update Keymaster.cs

--- Roles/Neutral/Keymaster.cs
+++ Roles/Neutral/Keymaster.cs
@@ -1,4 +1,4 @@
-﻿using AmongUs.GameOptions;
+using AmongUs.GameOptions;
 using Hazel;
 using InnerNet;
 using System.Text;
@@ -25,18 +25,38 @@ internal class Keymaster : RoleBase
 
     private static readonly Dictionary<byte, HashSet<byte>> KeyedList = [];
     private static OptionItem KeyGiveCooldown;
-    private static byte KeyID = 19;
+    private static byte KeyID = 10;
+    private static bool HasKey = false;
+    private static bool FOCUSED = false;
+    private static bool RED = false;
+    private static bool BLUE = false;
+    private static bool GREEN = false;
+    private static bool PINK = false;
+    private static bool CYAN = false;
+    private static bool YELLOW = false;
+    private static bool PURPLE = false;
+    private static bool LIME = false;
 
     public override void SetupCustomOption()
     {
         SetupRoleOptions(Id, TabGroup.NeutralRoles, CustomRoles.Keymaster);
-        KeyGiveCooldown = FloatOptionItem.Create(Id + 10, GeneralOption.KillCooldown, new(0, 300, 5), 300f, TabGroup.NeutralRoles, false).SetParent(CustomRoleSpawnChances[CustomRoles.Keymaster])
+        KeyGiveCooldown = FloatOptionItem.Create(Id + 10, GeneralOption.KillCooldown, new(0, 300, 5), 5f, TabGroup.NeutralRoles, false).SetParent(CustomRoleSpawnChances[CustomRoles.Keymaster])
             .SetValueFormat(OptionFormat.Seconds);
     }
     public override void Init()
     {
         playerIdList.Clear();
-        KeyID = 19;
+        HasKey = false;
+        KeyID = 10;
+        FOCUSED = false;
+        RED = false;
+        BLUE = false;
+        GREEN = false;
+        PINK = false;
+        CYAN = false;
+        YELLOW = false;
+        PURPLE = false;
+        LIME = false;
         KeyedList.Clear();
     }
     public override void Add(byte playerId)
@@ -76,90 +96,82 @@ internal class Keymaster : RoleBase
 
     public override void OnEnterVent(PlayerControl pc, Vent vent)
     {
-        pc.Notify(GetString("KeymasterRed"), 15f);
-        var rand = IRandom.Instance;
-        KeyID = (byte)rand.Next(1, 18);
-
-        switch (KeyID)
+        if (IsKeyedAll(pc))
         {
-            case 1:
-                pc.Notify(GetString("KeymasterRed"), 15f);
-                break;
-            case 2:
-                pc.Notify(GetString("KeymasterBlue"), 15f);
-                break;
-            case 3:
-                pc.Notify(GetString("KeymasterGreen"), 15f);
-                break;
-            case 4:
-                pc.Notify(GetString("KeymasterPink"), 15f);
-                break;
-            case 5:
-                pc.Notify(GetString("KeymasterOrange"), 15f);
-                break;
-            case 6:
-                pc.Notify(GetString("KeymasterYellow"), 15f);
-                break;
-            case 7:
-                pc.Notify(GetString("KeymasterBlack"), 15f);
-                break;
-            case 8:
-                pc.Notify(GetString("KeymasterWhite"), 15f);
-                break;
-            case 9:
-                pc.Notify(GetString("KeymasterPurple"), 15f);
-                break;
-            case 10:
-                pc.Notify(GetString("KeymasterBrown"), 15f);
-                break;
-            case 11:
-                pc.Notify(GetString("KeymasterCyan"), 15f);
-                break;
-            case 12:
-                pc.Notify(GetString("KeymasterLime"), 15f);
-                break;
-            case 13:
-                pc.Notify(GetString("KeymasterMaroon"), 15f);
-                break;
-            case 14:
-                pc.Notify(GetString("KeymasterRose"), 15f);
-                break;
-            case 15:
-                pc.Notify(GetString("KeymasterBanana"), 15f);
-                break;
-            case 16:
-                pc.Notify(GetString("KeymasterGray"), 15f);
-                break;
-            case 17:
-                pc.Notify(GetString("KeymasterTan"), 15f);
-                break;
-            case 18:
-                pc.Notify(GetString("KeymasterCoral"), 15f);
-                break;
-            default: // just in case
-                break;
+        HasKey = false;
+        }
+        else
+        {
+        HasKey = true;
+        pc.Notify(GetString("KeymasterGainedKey"), 5f);
         }
     }
-
     public override bool ForcedCheckMurderAsKiller(PlayerControl killer, PlayerControl target)
     {
         if (IsKeyed(killer.PlayerId, target.PlayerId))
         {
-            killer.Notify(GetString("TargetAlreadyKeyedShouldNotHappen"));
             return false;
         }
+        if (HasKey == true)
+        {
         KeyedList[killer.PlayerId].Add(target.PlayerId);
         SendRPC(killer, target);
         NotifyRoles(SpecifySeer: killer);
+        killer.Notify(GetString("KeymasterMarkedPlayer"), 5f);
 
         CheckKeyedAllPlayers();
 
         killer.ResetKillCooldown();
         killer.SetKillCooldown();
+        HasKey = false;
+        }
+        if (FOCUSED == true)
+        {
+        var rand = IRandom.Instance;
+        KeyID = (byte)rand.Next(1, 8);
 
+            switch (KeyID)
+            {
+                case 1:
+                    killer.Notify(GetString("KeymasterRed"), 15f);
+                    RED = true;
+                    break;
+                case 2:
+                    killer.Notify(GetString("KeymasterBlue"), 15f);
+                    BLUE = true;
+                    break;
+                case 3:
+                    killer.Notify(GetString("KeymasterGreen"), 15f);
+                    GREEN = true;
+                    break;
+                case 4:
+                    killer.Notify(GetString("KeymasterPink"), 15f);
+                    PINK = true;
+                    break;
+                case 5:
+                    killer.Notify(GetString("KeymasterCyan"), 15f);
+                    CYAN = true;
+                    break;
+                case 6:
+                    killer.Notify(GetString("KeymasterYellow"), 15f);
+                    YELLOW = true;
+                    break;
+                case 7:
+                    killer.Notify(GetString("KeymasterPurple"), 15f);
+                    PURPLE = true;
+                    break;
+                case 8:
+                    killer.Notify(GetString("KeymasterLime"), 15f);
+                    LIME = true;
+                    break;
+                default:
+                    break;
+            }
+            FOCUSED = false;
+            return false;
+        }
         return false;
     }
-
     private static bool IsKeyedAll(PlayerControl player)
     {
         if (!player.Is(CustomRoles.Keymaster)) return false;
@@ -184,14 +196,9 @@ internal class Keymaster : RoleBase
 
             if (IsKeyedAll(Keymaster))
             {
-                playerIdList.Remove(KeyedId);
-
-                Keymaster.GetRoleClass()?.OnAdd(KeyedId);
-
-                Keymaster.Notify(GetString("KeymasterFOCUS"), time: 2f);
                 Keymaster.RpcGuardAndKill(Keymaster);
                 Keymaster.ResetKillCooldown();
-
+                FOCUSED = true;
                 NotifyRoles(SpecifySeer: Keymaster);
                 Keymaster.MarkDirtySettings();
             }
